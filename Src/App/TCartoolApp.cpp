DenisBrunet/Cartool

commit 4fea6cccccacc5e17f77950de751940677265a2c

- TStrings: constructor and Set method to initialize from a vector<string>. - TGoF:   - Constructors would mostly call the TStrings constructors. The latter call the method "Add" all the time, but as it is overridden in TGoF, it is still unknown at TStrings construction, which result in calling the base Add method instead. We need to explicitly call the Set methods from TGoF to circumvent this, and call the overridden Add.   - Add now also calls CheckAbsolutePath, en plus of CheckExtendedPath, to make sure we always have fully qualified paths all the time. - CheckAbsolutePath: Windows utility that does all the job of converting a relative path to an absolute one for us! - TFileName:   - Added a flag to test for absolute path.   - Added CheckAbsolutePath method.   - CheckFileName test said flag and calls said method.   - Tested.

Src/Utils/Files.TGoF.cpp
@@ -203,20 +203,24 @@ return  filei2 - filei1 + 1;
 
 
 //----------------------------------------------------------------------------
-                                        // Overridden functions, called from either TGoF or TStrings
-                                        // It is assumed forcing a fixed size of MaxPathShort would provide enough room for all operations based on file names / paths
+                                        // Overridden functions, called from either TGoF or TStrings:
+                                        //  - Adding some margin to the reserved size, in case of extension replacement
+                                        //  - Making sure we have at least 2K space reserved
+                                        //  - Making sure we have absolute path files
+                                        //  - Checking file names bigger than old 256 MAX_PATH have the extended prefix
 void    TGoF::Add ( const char* string )
 {
-TStrings::Add ( string, StringFixedSize, max ( StringLength ( string ) + 64, (long) MaxPathShort ) ); 
-
-CheckExtendedPath ( Strings.GetLast () ); 
+Add ( string, 0 );
 }
 
 
 void    TGoF::Add ( const char* string, long length )
 {
-TStrings::Add ( string, StringFixedSize, max ( length, StringLength ( string ) + 64, (long) MaxPathShort ) ); 
+int                 stringlength    = max ( length, StringLength ( string ) + 64, (long) MaxPathShort ); 
 
+TStrings::Add ( string, StringFixedSize, stringlength ); 
+
+CheckAbsolutePath ( Strings.GetLast (), stringlength ); 
 CheckExtendedPath ( Strings.GetLast () ); 
 }
 

Src/Utils/Files.TGoF.h
@@ -156,13 +156,15 @@ class   TGoGoF;
 class   TGoF        :   public TStrings
 {
 public:
-                    TGoF ()                                     : TStrings   ()                             {}
-                    TGoF ( int numstrings )                     : TStrings   ( numstrings, MaxPathShort )   {}  // expected max size of file names
-                    TGoF ( const char*              string  )   : TStrings   ( string  )                    {}
-                    TGoF ( const TStrings*          strings )   : TStrings   ( strings )                    {}
-                    TGoF ( const TStrings&          strings )   : TStrings   ( strings )                    {}
-                    TGoF ( const TList<char>&       strings )   : TStrings   ( strings )                    {}
-                    TGoF ( const owl::TStringArray& strings )   : TStrings   ( strings )                    {}
+                                        // We override the Add method here, but base constructors know nothing about it, so we need to "duplicate" the TStrings construcotr bodies
+                    TGoF ()                                             : TStrings   ()            {}
+                    TGoF ( int numstrings )                             { Set ( numstrings, MaxPathShort ); } // expected max size of file names
+                    TGoF ( const char*                      string  )   { SetOnly ( string );       }
+                    TGoF ( const TStrings*                  strings )   { Set ( strings );          }
+                    TGoF ( const TStrings&                  strings )   { Set ( (TGoF &) strings ); }
+                    TGoF ( const std::vector<std::string>&  strings )   { Set ( strings );          }
+                    TGoF ( const TList<char>&               strings )   { Set ( strings );          }
+                    TGoF ( const owl::TStringArray&         strings )   { Set ( strings );          }
                     TGoF ( const owl::TDropInfo&      drop, const char* filesext = 0, TPointInt* where = 0, bool doesnotbelong = false );   // from drop info + extension filter + position
                     TGoF ( const TGoGoF& gogof, int gofi1 = -1, int gofi2 = -1 );   // Very handy trick to "flatten" a group of group of files
                     TGoF ( const TGoGoF& gogof, const TGoF& gof );                  // Very handy trick to "flatten" a group of group of files + another group
@@ -172,8 +174,8 @@ public:
     int             NumFiles        ( int filei1, int filei2 )  const;          // optional range that will be tested against current range
 
                                         // virtual functions
-    void            Add             ( const char* string );
-    void            Add             ( const char* string, long length );
+    void            Add             ( const char* string )              override;
+    void            Add             ( const char* string, long length ) override;
     void            Add             ( const TGoF& gof );
 
 

Src/Utils/Strings.TStrings.h
@@ -41,12 +41,13 @@ class   TStrings
 {
 public:
                     TStrings    ();
-                    TStrings    ( int numstrings, long stringlength )                             { Set ( numstrings, stringlength ); }
-                    TStrings    ( const char*         string  )                                   { SetOnly ( string ); }
-                    TStrings    ( const TStrings*     strings )                                   { Set ( strings ); }
-                    TStrings    ( const TList<char>&  strings )                                   { Set ( strings ); }
-                    TStrings    ( const owl::TStringArray& strings )                              { Set ( strings ); }
-                    TStrings    ( const TArray2<char>&  arraychar )                               { Set ( arraychar ); }
+                    TStrings    ( int numstrings, long stringlength )               { Set     ( numstrings, stringlength ); }
+                    TStrings    ( const char*                       string  )       { SetOnly ( string );                   }
+                    TStrings    ( const TStrings*                   strings )       { Set     ( strings );                  }
+                    TStrings    ( const std::vector<std::string>&   strings )       { Set     ( strings );                  }
+                    TStrings    ( const TList<char>&                strings )       { Set     ( strings );                  }
+                    TStrings    ( const owl::TStringArray&          strings )       { Set     ( strings );                  }
+                    TStrings    ( const TArray2<char>&              arraychar )     { Set     ( arraychar );                }
 
     virtual        ~TStrings    ();
 
@@ -67,11 +68,12 @@ public:
     virtual void    Reset           ();
 
     void            Set             ( int numstrings, long stringsize );
-    void            Set             ( const TStrings*    strings );
-    void            Set             ( const TStrings&    strings );
-    void            Set             ( const TList<char>& strings );
+    void            Set             ( const TStrings*                   strings );
+    void            Set             ( const TStrings&                   strings );
+    void            Set             ( const std::vector<std::string>&   strings );
+    void            Set             ( const TList<char>&                strings );
     void            Set             ( const char* strings, const char* separators );
-    void            Set             ( const owl::TStringArray& strings );   // OWL strings
+    void            Set             ( const owl::TStringArray&          strings );   // OWL strings
     void            Set             ( const TArray2<char>& arraychar );     // // "C"-like array of chars
     void            SetOnly         ( const char* string );
 

Src/Utils/Files.Utils.cpp
@@ -336,6 +336,17 @@ return  false;
 
 
 //----------------------------------------------------------------------------
+void        CheckAbsolutePath ( char* path, int maxsize )
+{
+if ( StringIsEmpty ( path ) )
+    return;
+
+
+_fullpath ( path, path, maxsize );
+}
+
+
+//----------------------------------------------------------------------------
                                         // check for very long filenames, and prepend the magic string if needed
 void        CheckExtendedPath ( char* path, bool force )
 {
@@ -1353,6 +1364,12 @@ return  CanOpenFile ( (char*) TFileName ( file ), flags );
 }
 
 
+bool    CanOpenFile ( const string& file, CanOpenFlags flags )
+{
+return  CanOpenFile ( (char*) TFileName ( file.c_str () ), flags );
+}
+
+
 //----------------------------------------------------------------------------
 bool    CanOpenFile ( char *file, CanOpenFlags flags )
 {
@@ -1507,7 +1524,7 @@ Reset ();
                                         // make a safe copy, in a big enough fixed buffer
 StringCopy ( FileName, filename, TFilenameSize - 1 );
 
-Check ( flags );
+CheckFileName ( flags );
 }
 
 
@@ -1526,32 +1543,38 @@ if ( StringIsNotEmpty ( ext ) )
 }
 
 
-void   TFileName::Check ( TFilenameFlags flags )
+//----------------------------------------------------------------------------
+void   TFileName::CheckFileName ( TFilenameFlags flags )
 {
                                         // Check path with this preferred sequence:
 
                                         // can expand to a longer string - though is currently prevented to be longer than 256
-if ( flags & TFilenameMsDosToWindows )
+if ( IsFlag ( flags, TFilenameMsDosToWindows ) )
     MsDosPathToWindowsPath ();
 
+                                        // convert a relative path to an absolute one
+if ( IsFlag ( flags, TFilenameAbsolutePath ) )
+    CheckAbsolutePath ();
+
                                         // add special prefix for path longer than 256
-if ( flags & TFilenameExtendedPath )
+if ( IsFlag ( flags, TFilenameExtendedPath ) )
     CheckExtendedPath ();
 
                                         // convert to another extension / sibling file for Cartool purpose (.img to .hdr f.ex.)
-if ( flags & TFilenameSibling )
+if ( IsFlag ( flags, TFilenameSibling ) )
     CheckSiblingFile ();
 
                                         // keeping only the directory part - trailing \ is removed
-if ( flags & TFilenameDirectory )
+if ( IsFlag ( flags, TFilenameDirectory ) )
     RemoveFilename ( FileName, false );
 
-                                        // avoiding averwrite by generating a new version name
-if ( flags & TFilenameNoOverwrite )
+                                        // avoiding overwrite by generating a variation of given file name
+if ( IsFlag ( flags, TFilenameNoOverwrite ) )
     CheckNoOverwrite ();
 }
 
 
+//----------------------------------------------------------------------------
         TFileName::TFileName ( const TFileName &op )
 {
 StringCopy          ( FileName, op.FileName, TFilenameSize - 1 );
@@ -1575,6 +1598,18 @@ return  *this;
 
 
 //----------------------------------------------------------------------------
+bool   TFileName::IsRelativePath  ()    const
+{
+return  PathIsRelative  ( FileName );   // !doc says up to MAX_PATH chars!
+}
+
+
+bool   TFileName::IsAbsolutePath  ()    const
+{
+return  ! IsRelativePath  (); 
+}
+
+
 bool   TFileName::IsExtendedPath  ()
 {
 return  crtl::IsExtendedPath ( FileName ); 
@@ -1604,6 +1639,12 @@ crtl::CheckExtendedPath ( FileName );
 void   TFileName::MsDosPathToWindowsPath ()
 {
 crtl::MsDosPathToWindowsPath ( FileName );
+}
+
+
+void   TFileName::CheckAbsolutePath ()
+{
+crtl::CheckAbsolutePath ( FileName, TFilenameSize );
 }
 
 

Src/Utils/Files.Utils.h
@@ -61,6 +61,7 @@ char*               AppendFilenameAsSubdirectory    ( char* path );
 bool                CreatePath                      ( const char* path, bool endswithfilename );  // make sure the whole chain of directories are created, up to the final file
 int                 FilenameToSiblings              ( char* file, TGoF &gof );              // transforms/expands a filename into its legal sibling (.img -> .hdr, MFF -> signal?.bin, etc..)
 void                CheckSiblingFile                ( char* path );
+void                CheckAbsolutePath               ( char* path, int maxsize );
 bool                IsExtendedPath                  ( char* path );
 void                CheckExtendedPath               ( char* path, bool force = false );     // checks if path needs the magic prefix "\\?\"
 void                CheckNoOverwrite                ( char* path );                         // prevent overwriting by making the current file a variation
@@ -158,8 +159,9 @@ enum                CanOpenFlags
                     };
 
 
-bool                CanOpenFile      ( char*        file,   CanOpenFlags flags = CanOpenFileDefault );  // !can update path parameter!
-bool                CanOpenFile      ( const char*  file,   CanOpenFlags flags = CanOpenFileDefault );
+bool                CanOpenFile      ( char*              file,   CanOpenFlags flags = CanOpenFileDefault );  // !can update path parameter!
+bool                CanOpenFile      ( const char*        file,   CanOpenFlags flags = CanOpenFileDefault );
+bool                CanOpenFile      ( const std::string& file,   CanOpenFlags flags = CanOpenFileDefault );
 
 
 //----------------------------------------------------------------------------
@@ -179,10 +181,11 @@ enum                TFilenameFlags
                     {
                     TFilenameNoPreprocessing    = 0x000,
                     TFilenameMsDosToWindows     = 0x001,
-                    TFilenameExtendedPath       = 0x002,
-                    TFilenameSibling            = 0x004,
-                    TFilenameDirectory          = 0x008,
-                    TFilenameNoOverwrite        = 0x010,
+                    TFilenameAbsolutePath       = 0x002,
+                    TFilenameExtendedPath       = 0x004,
+                    TFilenameSibling            = 0x008,
+                    TFilenameDirectory          = 0x010,
+                    TFilenameNoOverwrite        = 0x020,
                     };
 
 
@@ -204,9 +207,11 @@ public:
     void            SetTempFileName         ( const char* ext );
 
 
-    bool            IsEmpty                 ()      const                   { return  StringIsEmpty    ( FileName ) ; }
-    bool            IsNotEmpty              ()      const                   { return  StringIsNotEmpty ( FileName ) ; }
+    bool            IsEmpty                 ()      const                   { return  StringIsEmpty    ( FileName ); }
+    bool            IsNotEmpty              ()      const                   { return  StringIsNotEmpty ( FileName ); }
 
+    bool            IsRelativePath          ()      const;
+    bool            IsAbsolutePath          ()      const;
 
     bool            IsExtendedPath          ();
     void            ResetExtendedPath       ();
@@ -214,9 +219,10 @@ public:
 
 
     bool            CanOpenFile             ()  const;
-    void            Check                   ( TFilenameFlags flags );
+    void            CheckFileName           ( TFilenameFlags flags );
     void            CheckExtendedPath       ();
     void            MsDosPathToWindowsPath  ();
+    void            CheckAbsolutePath       ();
     void            CheckSiblingFile        ();
     void            CheckNoOverwrite        ();
     void            GetCurrentDir           ()                      { ::GetCurrentDirectory ( Size (), FileName ); }

Src/Utils/Strings.TStrings.cpp
@@ -189,6 +189,13 @@ Set ( &strings );
 }
 
 
+void    TStrings::Set ( const vector<string>&    strings )
+{
+for ( int i = 0; i < strings.size (); i++ )
+    Add ( strings[ i ].c_str () );
+}
+
+
 void    TStrings::Set ( const TList<char>& strings )
 {
 if ( &Strings == &strings )

Src/Volumes/TExportVolume.cpp
@@ -172,7 +172,7 @@ if ( Filename.IsEmpty () ) {            // no filename is kind of a problem...
     }
 
                                         // can modify Filename
-Filename.Check ( (TFilenameFlags) ( TFilenameExtendedPath | TFilenameSibling ) );
+Filename.CheckFileName ( (TFilenameFlags) ( TFilenameExtendedPath | TFilenameSibling ) );
 
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

commit 109a5f7598347f902028939961421b70f5789195

TGoF constructor glitch: - Commit 4fea6cccccacc5e17f77950de751940677265a2c introduced a side-effect, where if a TGoF were to contain non-file strings (so actually a TStrings in disguise), it would be "upgraded" to a full-path file name in the constructor. Which does not make any sense in this case. Fortunately, programmer 6th sense was to not publish this release... - TGoF:   - CheckFileNames: new method to wrap call TFileName::CheckFileName with the corresponding check flags, so we can rely on existing code. Another nicety is the in-place string replacement from the TList.   - Add main method does not call CheckAbsolutePath anymore, which was the culprit.   - Upgraded the vector<string> constructor with an optional check flags parameter, and calling new method CheckFileNames accordingly. - TCartoolApp::InitInstance CLI initialization makes use of new flags by checking both absolute and extended paths. Note that this is the only instance of using this TGoF constructor. - CLI tested.

Src/App/TCartoolApp.cpp
@@ -742,7 +742,7 @@ catch ( const CLI::ParseError &e ) {
     }
 
                                         // For our own convenience, convert vector<string> files to TGoF, while also converting any relative path to absolute
-TGoF                gof ( files );
+TGoF                gof ( files, (TFilenameFlags) ( TFilenameAbsolutePath, TFilenameExtendedPath ) );
 
 
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -

Src/Utils/Files.TGoF.cpp
@@ -220,7 +220,6 @@ int                 stringlength    = max ( length, StringLength ( string ) + 64
 
 TStrings::Add ( string, StringFixedSize, stringlength ); 
 
-CheckAbsolutePath ( Strings.GetLast (), stringlength ); 
 CheckExtendedPath ( Strings.GetLast () ); 
 }
 
@@ -228,6 +227,35 @@ CheckExtendedPath ( Strings.GetLast () );
 void    TGoF::Add ( const TGoF& gof )                             
 {
 TStrings::Add ( gof );               // will call back our TGoF::Add above
+}
+
+
+//----------------------------------------------------------------------------
+void    TGoF::CheckFileNames  ( TFilenameFlags flags )
+{
+if ( flags == TFilenameNoPreprocessing )
+    return;
+
+
+for ( int i = 0; i < (int) Strings; i++ ) {
+                                        // copy AND update file name at the same time
+    TFileName   filename ( Strings[ i ], flags );
+
+                                        // any effective change?
+    if ( StringIs ( filename, Strings[ i ] ) )
+        continue;
+
+                                        // allocate a new string
+    int         stringsize      = max ( filename.Length () + 64, (long) MaxPathShort ); 
+    char*       toc             = new char [ stringsize ];
+
+    ClearString ( toc, stringsize );
+    StringCopy  ( toc, filename   );
+
+                                        // delete old string, without releasing the list atom, and make it point to the new memory string instead
+    delete[]    Strings.GetAtom ( i )->To;
+    Strings.GetAtom ( i )->To = toc;
+    }
 }
 
 

Src/Utils/Files.TGoF.h
@@ -157,12 +157,13 @@ class   TGoF        :   public TStrings
 {
 public:
                                         // We override the Add method here, but base constructors know nothing about it, so we need to "duplicate" the TStrings construcotr bodies
+                                        // !Be sure to override all constructors from TStrings!
                     TGoF ()                                             : TStrings   ()            {}
                     TGoF ( int numstrings )                             { Set ( numstrings, MaxPathShort ); } // expected max size of file names
                     TGoF ( const char*                      string  )   { SetOnly ( string );       }
                     TGoF ( const TStrings*                  strings )   { Set ( strings );          }
                     TGoF ( const TStrings&                  strings )   { Set ( (TGoF &) strings ); }
-                    TGoF ( const std::vector<std::string>&  strings )   { Set ( strings );          }
+                    TGoF ( const std::vector<std::string>&  strings, TFilenameFlags flags = TFilenameNoPreprocessing )  { Set ( strings ); CheckFileNames ( flags );         }
                     TGoF ( const TList<char>&               strings )   { Set ( strings );          }
                     TGoF ( const owl::TStringArray&         strings )   { Set ( strings );          }
                     TGoF ( const owl::TDropInfo&      drop, const char* filesext = 0, TPointInt* where = 0, bool doesnotbelong = false );   // from drop info + extension filter + position
@@ -177,6 +178,8 @@ public:
     void            Add             ( const char* string )              override;
     void            Add             ( const char* string, long length ) override;
     void            Add             ( const TGoF& gof );
+
+    void            CheckFileNames  ( TFilenameFlags flags );
 
 
     bool            AllExtensionsAre            ( const char* exts, int atomtype = 0 /*UnknownAtomType*/ )    const;  // all files have extensions belonging to the list provided
@@ -223,7 +226,7 @@ public:
     bool            GetCommonString ( char* base, bool includedir = true, bool includedifference = false )      const;
     void            GetFilenamesSubRange    ( int& fromchars, int& tochars )                                    const;
 
-    void            Sort            (); // overriding TStrings::Sort
+    void            Sort            ()  override; // overriding TStrings::Sort
 
     void            SplitFreqFiles  ( SplitFreqFlags how, TGoGoF *gogofout = 0, bool showgauge = true );
     void            Resample        ( ResamplingType resampling, int numresamples, int resamplingsize, const TGoF* gofalt, TGoGoF& resgogof, TGoGoF* resgogofalt );  // resampling into anoth group of files

Src/Utils/Strings.TStrings.h
@@ -121,9 +121,9 @@ public:
     TStrings&       operator    =       ( const TStrings &op2 );
 
 
-    char*           operator    []              ( int index )       { return Strings[ index ]; }
+          char*     operator    []              ( int index )       { return Strings[ index ]; }
     const char*     operator    []              ( int index ) const { return Strings[ index ]; }    // access from const object forces returning a const char*
-    char*           operator    ()              ( int index )       { return Strings[ index ]; }
+          char*     operator    ()              ( int index )       { return Strings[ index ]; }
     const char*     operator    ()              ( int index ) const { return Strings[ index ]; }    // access from const object forces returning a const char*
 
                     operator    int                 ()  const       { return (int)  Strings; }