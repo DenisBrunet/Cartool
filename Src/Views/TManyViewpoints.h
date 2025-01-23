05377674c2aab9a2c282f6e50a9d0d40fd07552d
DenisBrunet <Denis.Brunet@unige.ch>
2025-01-23 11:38:24 +0100

    Fixed the copyright notice to refer to the University of Geneva, Switzerland, rather than the CIBM entity.

--- Src/Views/TManyViewpoints.h
+++ Src/Views/TManyViewpoints.h
@@ -1,6 +1,6 @@
 /*
 ==========================================================================
-Copyright 2024 CIBM (Center for Biomedical Imaging), Lausanne, Switzerland
+© 2024-2025 Denis Brunet, University of Geneva, Switzerland.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
