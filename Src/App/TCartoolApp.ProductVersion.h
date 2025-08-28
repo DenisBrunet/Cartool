3a595946f9634ffad0e4ba3650b82e538a604e20
DenisBrunet <Denis.Brunet@unige.ch>
2025-08-28 15:02:04 +0200

    - Bumped patch version.

--- Src/App/TCartoolApp.ProductVersion.h
+++ Src/App/TCartoolApp.ProductVersion.h
@@ -21,5 +21,5 @@ limitations under the License.
 #define		MajorRevisionStr    "5"
 #define		MinorRevisionInt     06
 #define		MinorRevisionStr    "06"
-#define		PatchRevisionInt     04
-#define		PatchRevisionStr    "04"
+#define		PatchRevisionInt     05
+#define		PatchRevisionStr    "05"
